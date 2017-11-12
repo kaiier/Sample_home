f7635da6912da07d83026f4e3af78eb44c448af1
vagrant <vagrant@homestead>
2017-11-12 10:28:53 +0000

    增加blade模板

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+@extends('layouts.default')
+@section('kaii')
+ <h1>关于页</h1>
+ @stop 
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+@extends('layouts.default')
+@section('kaii')
+ <h1>帮助页</h1>
+ @stop
